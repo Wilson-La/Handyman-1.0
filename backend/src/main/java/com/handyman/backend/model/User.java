d81fefe3245433b29444adf89874e043b3b2a9c7
Wilson <wilsonla0920@gmail.com>
2022-12-13 22:35:01 -0500

    Created user and role entity along with repository interfaces for both

--- backend/src/main/java/com/handyman/backend/model/User.java
+++ backend/src/main/java/com/handyman/backend/model/User.java
@@ -2,25 +2,83 @@ package com.handyman.backend.model;
 
 import jakarta.persistence.*;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
+import static jakarta.persistence.GenerationType.AUTO;
+
 @Entity
-@Table(name="user", uniqueConstraints = {
-        @UniqueConstraint(columnNames = "username")
-})
 public class User {
 
     @Id
-    @Column(name="userid")
-    private int id;
-    @Column(name="username")
-    @Basic
+    @GeneratedValue(strategy = AUTO)
+    private Long id;
+    private String name;
     private String username;
-    @Column(name="password")
-    @Basic
-    private String password;
 
-    public User(String username, String password){
+    private String password;
+    @ManyToMany(fetch = FetchType.EAGER)
+    private Collection<Role> roles = new ArrayList<>();
+
+    public User(String name, String username, String password){
+        this.name = name;
         this.username = username;
         this.password = password;
     }
 
+    public User() {
+        this.name = null;
+        this.username = null;
+        this.password = null;
+    }
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Collection<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Collection<Role> roles) {
+        this.roles = roles;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", roles=" + roles +
+                '}';
+    }
 }
--- backend/src/main/java/com/handyman/backend/repository/UserRepository.java
+++ backend/src/main/java/com/handyman/backend/repository/UserRepository.java
@@ -1,4 +1,9 @@
 package com.handyman.backend.repository;
 
-public interface UserRepository {
+import com.handyman.backend.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+
 }
