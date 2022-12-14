2d11fb444adbe6be8be03efb72a401b204042b26
Wilson <wilsonla0920@gmail.com>
2022-12-13 22:49:42 -0500

    Started coding UserServiceImplementation

--- backend/src/main/java/com/handyman/backend/service/UserService.java
+++ backend/src/main/java/com/handyman/backend/service/UserService.java
@@ -1,4 +1,14 @@
 package com.handyman.backend.service;
 
+import com.handyman.backend.model.Role;
+import com.handyman.backend.model.User;
+
+import java.util.List;
+
 public interface UserService {
+    User saveUser(User user);
+    Role saveRole(Role role);
+    void addRoleToUser(String username, String roleName);
+    User getUser(String username);
+    List<User> getUsers();
 }
--- backend/src/main/java/com/handyman/backend/service/UserServiceImpl.java
+++ backend/src/main/java/com/handyman/backend/service/UserServiceImpl.java
@@ -1,5 +1,46 @@
 package com.handyman.backend.service;
 
-public class UserServiceImpl {
+import com.handyman.backend.model.Role;
+import com.handyman.backend.model.User;
+import com.handyman.backend.repository.RoleRepository;
+import com.handyman.backend.repository.UserRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+@Service
+@Transactional
+public class UserServiceImpl implements UserService{
+    private final UserRepository userRepo;
+    private final RoleRepository roleRepo;
+
+    public UserServiceImpl(UserRepository userRepo, RoleRepository roleRepo) {
+        this.userRepo = userRepo;
+        this.roleRepo = roleRepo;
+    }
+
+    @Override
+    public User saveUser(User user) {
+        return null;
+    }
+
+    @Override
+    public Role saveRole(Role role) {
+        return null;
+    }
+
+    @Override
+    public void addRoleToUser(String username, String roleName) {
+
+    }
+
+    @Override
+    public User getUser(String username) {
+        return null;
+    }
+
+    @Override
+    public List<User> getUsers() {
+        return null;
+    }
 }
