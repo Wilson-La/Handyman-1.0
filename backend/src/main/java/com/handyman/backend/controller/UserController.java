f1c1e11a9e3de8298b6daeec30440200aaa52074
Wilson <wilsonla0920@gmail.com>
2022-12-14 19:10:14 -0500

    finished the userServiceImpl class and started working on the userController for API endpoints

--- backend/src/main/java/com/handyman/backend/controller/UserController.java
+++ backend/src/main/java/com/handyman/backend/controller/UserController.java
@@ -1,4 +1,23 @@
 package com.handyman.backend.controller;
 
+import com.handyman.backend.model.User;
+import com.handyman.backend.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
 public class UserController {
+    private final UserService userService;
+
+    @GetMapping("/users")
+    public ResponseEntity<List<User>> getUsers(){
+        return ResponseEntity.ok().body(userService.getUsers());
+    };
 }
--- backend/src/main/java/com/handyman/backend/service/UserServiceImpl.java
+++ backend/src/main/java/com/handyman/backend/service/UserServiceImpl.java
@@ -5,11 +5,13 @@ import com.handyman.backend.model.User;
 import com.handyman.backend.repository.RoleRepository;
 import com.handyman.backend.repository.UserRepository;
 import jakarta.transaction.Transactional;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 @Service
 @Transactional
+@Slf4j
 public class UserServiceImpl implements UserService{
     private final UserRepository userRepo;
     private final RoleRepository roleRepo;
@@ -21,26 +23,33 @@ public class UserServiceImpl implements UserService{
 
     @Override
     public User saveUser(User user) {
-        return null;
+        log.info("Saving new user {} to the database", user.getName());
+        return userRepo.save(user);
     }
 
     @Override
     public Role saveRole(Role role) {
-        return null;
+        log.info("Saving new role {} to the database", role.getName());
+        return roleRepo.save(role);
     }
 
     @Override
     public void addRoleToUser(String username, String roleName) {
-
+        log.info("Adding role {} to user {}", roleName, username);
+        User user = userRepo.findByUsername(username);
+        Role role = roleRepo.findByName(roleName);
+        user.getRoles().add(role);
     }
 
     @Override
     public User getUser(String username) {
-        return null;
+        log.info("fetching user {} ", username);
+        return userRepo.findByUsername(username);
     }
 
     @Override
     public List<User> getUsers() {
-        return null;
+        log.info("fetching all users");
+        return userRepo.findAll();
     }
 }
