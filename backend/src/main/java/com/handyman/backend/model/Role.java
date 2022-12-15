8a51c0f553311445b91767539b5a3b38b60fe10a
Wilson <wilsonla0920@gmail.com>
2022-12-15 00:03:12 -0500

    Created POST and GET APIS and tested them using test data in the Application class

--- backend/src/main/java/com/handyman/backend/model/Role.java
+++ backend/src/main/java/com/handyman/backend/model/Role.java
@@ -1,9 +1,15 @@
 package com.handyman.backend.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import static jakarta.persistence.GenerationType.AUTO;
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Role {
 
     @Id
@@ -11,36 +17,4 @@ public class Role {
     @GeneratedValue(strategy = AUTO)
     private Long id;
     private String name;
-
-    public Role(Long id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-
-    public Role() {
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    @Override
-    public String toString() {
-        return "Role{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                '}';
-    }
 }
--- backend/src/main/java/com/handyman/backend/model/User.java
+++ backend/src/main/java/com/handyman/backend/model/User.java
@@ -1,6 +1,9 @@
 package com.handyman.backend.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -8,6 +11,9 @@ import java.util.Collection;
 import static jakarta.persistence.GenerationType.AUTO;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
 
     @Id
@@ -15,70 +21,7 @@ public class User {
     private Long id;
     private String name;
     private String username;
-
     private String password;
     @ManyToMany(fetch = FetchType.EAGER)
     private Collection<Role> roles = new ArrayList<>();
-
-    public User(String name, String username, String password){
-        this.name = name;
-        this.username = username;
-        this.password = password;
-    }
-
-    public User() {
-        this.name = null;
-        this.username = null;
-        this.password = null;
-    }
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public Collection<Role> getRoles() {
-        return roles;
-    }
-
-    public void setRoles(Collection<Role> roles) {
-        this.roles = roles;
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", username='" + username + '\'' +
-                ", password='" + password + '\'' +
-                ", roles=" + roles +
-                '}';
-    }
 }
